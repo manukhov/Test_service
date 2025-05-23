ee029baa3db7bb6b05cf2ad7770f4078520f5dd8
manukhov.v <manukhov.v@agile-sys.ru>
2025-05-23 23:19:42 +0300

    Revert karma

--- frontend/src/karma.conf.js
+++ frontend/src/karma.conf.js
@@ -25,7 +25,16 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
-    singleRun: false
+    
+    // Используем кастомный браузер для CI
+    browsers: ['ChromeHeadlessNoSandbox'],
+    singleRun: true,
+
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu']
+      }
+    }
   });
 };
