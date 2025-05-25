df6c69d2d4cb31c2a68cd19a8b3bedb2e9bd6379
manukhov.v <manukhov.v@agile-sys.ru>
2025-05-25 23:56:15 +0300

    Fix CI

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,23 +1,44 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { EcommerceComponent } from './ecommerce/ecommerce.component';
+import { ProductsComponent } from './ecommerce/products/products.component';
+import { ShoppingCartComponent } from './ecommerce/shopping-cart/shopping-cart.component';
+import { OrdersComponent } from './ecommerce/orders/orders.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { EcommerceService } from './ecommerce/services/EcommerceService';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        EcommerceComponent,
+        ProductsComponent,
+        ShoppingCartComponent,
+        OrdersComponent
       ],
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule
+      ],
+      providers: [EcommerceService]
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
+
   it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app');
   }));
+
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
--- frontend/src/app/ecommerce/ecommerce.component.spec.ts
+++ frontend/src/app/ecommerce/ecommerce.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { EcommerceComponent } from './ecommerce.component';
+import { ProductsComponent } from './products/products.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { OrdersComponent } from './orders/orders.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { EcommerceService } from './services/EcommerceService';
 
 describe('EcommerceComponent', () => {
   let component: EcommerceComponent;
@@ -8,7 +13,18 @@ describe('EcommerceComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EcommerceComponent ]
+      declarations: [
+        EcommerceComponent,
+        ProductsComponent,
+        ShoppingCartComponent,
+        OrdersComponent
+      ],
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule
+      ],
+      providers: [EcommerceService]
     })
     .compileComponents();
   }));
